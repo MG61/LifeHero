8ab28d57cca85caf3c558814184bc2cab51df9d1
Nikita Abramenko <abramenko0431@gmail.com>
2025-05-10 19:02:55 +0300

    Android Support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.lifehero"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,10 +27,12 @@ android {
         applicationId = "com.example.lifehero"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+//        minSdk = flutter.minSdkVersion
+//        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        minSdk = 23
+        targetSdk = 33
     }
 
     buildTypes {
